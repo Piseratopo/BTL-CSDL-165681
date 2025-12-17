03b4e90f45f6183165d817c81984ee4b5b2a2e82
Piseratopo <buinhatminh2k52005@gmail.com>
2025-12-17 23:15:34 +0700

    change tab size

--- models.c
+++ models.c
@@ -31,50 +31,50 @@ Alarm* createAlarm(int id, const char* tag, const char* time, int priority, cons
  * @param head A pointer to the head of the linked list.
  */
 void inputAlarm(Alarm** head) {
-    int id, priority;
-    char buffer[128];          // general input buffer
-    char tag[20], time[20], description[100];
+   int id, priority;
+   char buffer[128];          // general input buffer
+   char tag[20], time[20], description[100];
 
-    printf("Alarm's ID: ");
-    if (fgets(buffer, sizeof(buffer), stdin)) {
-        id = (int)strtol(buffer, NULL, 10);
-    }
+   printf("Alarm's ID: ");
+   if (fgets(buffer, sizeof(buffer), stdin)) {
+      id = (int)strtol(buffer, NULL, 10);
+   }
 
-    printf("Tag (device name or channel): ");
-    if (fgets(tag, sizeof(tag), stdin)) {
-        // remove newline character from end of string
-        tag[strcspn(tag, "\n")] = '\0';
-    }
+   printf("Tag (device name or channel): ");
+   if (fgets(tag, sizeof(tag), stdin)) {
+      // remove newline character from end of string
+      tag[strcspn(tag, "\n")] = '\0';
+   }
 
-    printf("Time of occurrence: ");
-    if (fgets(time, sizeof(time), stdin)) {
-        time[strcspn(time, "\n")] = '\0';
-    }
+   printf("Time of occurrence: ");
+   if (fgets(time, sizeof(time), stdin)) {
+      time[strcspn(time, "\n")] = '\0';
+   }
 
-    printf("Priority (1: high, 2: medium, 3: low): ");
-    if (fgets(buffer, sizeof(buffer), stdin)) {
-        priority = (int)strtol(buffer, NULL, 10);
-    }
+   printf("Priority (1: high, 2: medium, 3: low): ");
+   if (fgets(buffer, sizeof(buffer), stdin)) {
+      priority = (int)strtol(buffer, NULL, 10);
+   }
 
-    printf("Short description: ");
-    if (fgets(description, sizeof(description), stdin)) {
-        description[strcspn(description, "\n")] = '\0';
-    }
+   printf("Short description: ");
+   if (fgets(description, sizeof(description), stdin)) {
+      description[strcspn(description, "\n")] = '\0';
+   }
 
-    // create a new alarm node and add to linked list
-    Alarm* newAlarm = createAlarm(id, tag, time, priority, description);
+   // create a new alarm node and add to linked list
+   Alarm* newAlarm = createAlarm(id, tag, time, priority, description);
 
-    if (*head == NULL) {
-        // if list is empty, set new alarm as head
-        *head = newAlarm;
-    } else {
-        Alarm* temp = *head;
-        // traverse list to find last node and append new node
-        while (temp->next != NULL) {
-            temp = temp->next;
-        }
-        temp->next = newAlarm;
-    }
+   if (*head == NULL) {
+      // if list is empty, set new alarm as head
+      *head = newAlarm;
+   } else {
+      Alarm* temp = *head;
+      // traverse list to find last node and append new node
+      while (temp->next != NULL) {
+         temp = temp->next;
+      }
+      temp->next = newAlarm;
+   }
 }
 
 /**
