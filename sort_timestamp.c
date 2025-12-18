22c74509bf5a2234ad50542e8258b83fda1ae4e4
thanhtien12-lab <nguyenfender@gmail.com>
2025-12-18 21:35:50 +0700

    Add files via upload

--- sort_timestamp.c
+++ sort_timestamp.c
@@ -12,17 +12,3 @@ void sortByTimestamp(Sample arr[], int n) {
         }
     }
 }
-// void sortByValue(Sample arr[], int n) {
-    //for (int i = 0; i < n - 1; i++) {
-      //  int minIdx = i;
-        //for (int j = i + 1; j < n; j++) {
-          //  if (arr[j].value < arr[minIdx].value)
-            //    minIdx = j;
-        //}
-        //if (minIdx != i) {
-          //  Sample tmp = arr[i];
-            //arr[i] = arr[minIdx];
-            //arr[minIdx] = tmp;
-        //}
-    //}
-//}
---
+++ tree.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "tree.h"
+
+TreeNode* createTreeNode(Sample s) {
+    TreeNode *node = (TreeNode*)malloc(sizeof(TreeNode));
+    node->data = s;
+    node->left = node->right = NULL;
+    return node;
+}
+
+TreeNode* insertBST(TreeNode *root, Sample s) {
+    if (root == NULL)
+        return createTreeNode(s);
+
+    if (strcmp(s.tag, root->data.tag) < 0)
+        root->left = insertBST(root->left, s);
+    else
+        root->right = insertBST(root->right, s);
+
+    return root;
+}
+
+TreeNode* searchBST(TreeNode *root, const char *tag) {
+    if (root == NULL) return NULL;
+
+    int cmp = strcmp(tag, root->data.tag);
+    if (cmp == 0) return root;
+    if (cmp < 0) return searchBST(root->left, tag);
+    return searchBST(root->right, tag);
+}
+
+void inorderPrint(TreeNode *root) {
+    if (root == NULL) return;
+
+    inorderPrint(root->left);
+    printf("%s | %s | %.2f %s\n",
+           root->data.timestamp,
+           root->data.tag,
+           root->data.value,
+           root->data.unit);
+    inorderPrint(root->right);
+}
